3a18be3c8139a07f85a7f5f9dfb4b7819db60320
guillaumeducuing <guillaumeducuing@gmail.com>
2025-10-09 17:01:32 +0200

    feat: add landing

---
+++ src/app/[lang]/landing/[uid]/page.tsx
@@ -0,0 +1,74 @@
+import { Metadata } from "next";
+import { SliceZone } from "@prismicio/react";
+
+import { PageService } from "@/services/page.service";
+
+import { generateMetadata as fetchMetadata } from "@/utils/generateMetadata";
+
+import { components as componentsHeros } from "@/sections/heroes";
+import { components as componentsSlices } from "@/sections/slices";
+
+import Layout from "@/components/Layout";
+
+import { LandingDocument } from "@/prismicio-types";
+
+import { notFound } from "next/navigation";
+
+export async function generateMetadata({
+  params
+}: {
+  params: Promise<{ lang: string }>;
+}): Promise<Metadata> {
+  const resolvedParams = await params;
+
+  return fetchMetadata({ params: resolvedParams, currentPage: "landing" });
+}
+
+export default async function Landings({
+  params
+}: {
+  params: Promise<{ lang: string; uid: string }>;
+}) {
+  const resolvedParams = await params;
+  const { lang, uid } = resolvedParams;
+
+  const pageService = new PageService(lang);
+
+  const [page] = await Promise.all([
+    pageService.getPageByUID({ documentType: "landing", uid: uid })
+  ]);
+  if (!page) {
+    notFound();
+  }
+
+  const { data } = page as LandingDocument;
+
+  return (
+    <Layout
+      lang={lang}
+      altLang={page.alternate_languages}
+      currentPage={page.type}
+    >
+      <SliceZone
+        slices={data.slices}
+        components={{
+          ...componentsHeros,
+          ...componentsSlices
+        }}
+      />
+      <></>
+    </Layout>
+  );
+}
+
+export async function generateStaticParams() {
+  const pageService = new PageService("*");
+
+  const pages = await pageService.getAllPageByTypes({
+    documentType: "landing"
+  });
+
+  return pages.map(page => {
+    return { uid: page.uid, lang: page.lang };
+  });
+}
--- src/prismicio.ts
+++ src/prismicio.ts
@@ -20,7 +20,7 @@ const routes: prismic.ClientConfig["routes"] = [
     path: "/:lang?"
   },
   {
-    type: "legals",
+    type: "landing",
     path: "/:lang?/:uid"
   }
 ];
