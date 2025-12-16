34b27544d4cd412f8395787e73fe622c83ff7a1a
raphael-m-prismic <raphael.mendes@prismic.io>
2025-12-16 17:17:00 +0100

    feat: added icons, svgs and new NextStep section

--- src/app/[lang]/recommandations/[uid]/page.tsx
+++ src/app/[lang]/recommandations/[uid]/page.tsx
@@ -1,13 +1,20 @@
 import { Metadata } from "next";
-
 import { generateMetadata as fetchMetadata } from "@/utils/generateMetadata";
-
 import { RecapDocument } from "@/prismicio-types";
 import { createClient } from "@/prismicio";
-import Container from "@/components/Container";
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
 import { isFilled } from "@prismicio/client";
+
+import Container from "@/components/Container";
+import Zoom from "@/components/Icons/Zoom";
+import Reuse from "@/components/Icons/Reuse";
+import Hammer from "@/components/Icons/Hammer";
+import { Try } from "./Try";
+import { Animation } from "./Animation";
+import { TopAnimation } from "./TopAnimation";
+import { BottomAnimation } from "./BottomAnimation";
+
 
 export async function generateMetadata({
   params,
@@ -33,80 +40,194 @@ export default async function Home({
   ]);
 
   const { data } = page as RecapDocument;
+  console.log(data)
 
-  console.log(page)
+  // map of icon components
+  const iconsMap: Record<string, React.FC<{ className?: string }>> = {
+    zoom: Zoom,
+    reuse: Reuse,
+    hammer: Hammer,
+  };
 
   return (
     <>
       {/* Hero */}
-      <section className="bg-[#FFFFFF] py-[80px]">
-        <Container size="xl" className="flex flex-col items-center text-center gap-16">
-          <div className="flex flex-col items-center text-center gap-4">
-            <PrismicNextImage field={data.client_logo} height={48} />
-            <p>{data.hero_eyebrow}</p>
-          </div>
-          <div className="flex flex-col items-center text-center gap-6 max-w-[700px]">
-            <h1 className="text-6xl font-bold">{data.hero_title}</h1>
-            <PrismicRichText field={data.hero_text} />
-          </div>
-          <div className="px-8 py-4 rounded-2xl bg-[#F7F7F7]">
-            <div className="flex justify-center items-center gap-6">
-              {isFilled.contentRelationship(data.contact) && (
-                <PrismicNextImage field={data.contact.data?.image} width={64} height={64} className="object-cover aspect-square rounded-full" />
+      <section id="hero" className="relative bg-[#FFFFFF] pt-[80px] pb-[200px] md:pb-[300px]">
+        <div className="absolute bottom-0 left-0 w-full ">
+          <TopAnimation></TopAnimation>
+        </div>
+        <div className="relative">
+          <Container size="xl" className="flex flex-col items-center text-center gap-16">
+            <div className="flex flex-col items-center text-center gap-4">
+              <PrismicNextImage field={data.client_logo} height={48} />
+              <p>{data.hero_eyebrow}</p>
+            </div>
+            <div className="flex flex-col items-center text-center gap-6 max-w-[700px]">
+
+              {isFilled.richText(data.hero_title) && (
+                <PrismicRichText 
+                  field={data.hero_title}
+                  components={{
+                    heading1: ({ children }) => (
+                      <h1 className="text-6xl font-bold">{children}</h1>
+                    ),
+                  }}
+                />
               )}
-              <div className="flex flex-col justify-center items-start">
+              
+              {isFilled.richText(data.hero_text) && (
+                <PrismicRichText 
+                    field={data.hero_text} 
+                    components={{
+                      paragraph: ({ children }) => <p className="text-[#505050]">{children}</p>,
+                  }}/>
+              )}
+            </div>
+
+            <div className="px-8 py-4 rounded-2xl bg-[#F7F7F7]">
+              <div className="flex flex-col md:flex-row justify-center items-center gap-6">
                 {isFilled.contentRelationship(data.contact) && (
-                  <p className="font-semibold">{data.contact.data?.name}</p>
+                  <PrismicNextImage 
+                    field={data.contact.data?.image} 
+                    width={64} 
+                    height={64} 
+                    className="object-cover aspect-square rounded-full" 
+                  />
                 )}
-                {isFilled.contentRelationship(data.contact) && (
-                  <p>{data.contact.data?.position}</p>
-                )}
+                <div className="flex flex-col justify-center items-start">
+                  {isFilled.contentRelationship(data.contact) && (
+                    <p className="font-semibold">{data.contact.data?.name}</p>
+                  )}
+                  {isFilled.contentRelationship(data.contact) && (
+                    <p>{data.contact.data?.position}</p>
+                  )}
+                </div>
+                <div className="flex justify-center items-center gap-4">
+                  {isFilled.contentRelationship(data.contact) && (
+                    <a href={`mailto:${data.contact.data?.email}`} className="text-[#FFFFFF] bg-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515]">
+                      Email me
+                    </a>
+                  )}
+                  {isFilled.contentRelationship(data.contact) && (
+                    <a href={`${data.contact.data?.calendar}`} className="text-[#151515] px-4 py-2 rounded-lg border-2 border-[#151515]">
+                      Book a call
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </Container>
+          </Container>
+          <Animation></Animation>
+        </div>
       </section>
-
+      
       {/* Context */}
-      <section className="bg-[#151515] py-[80px]">
+      <section id="context" className="bg-[#151515] py-[60px]">
         <Container size="xl" className="flex flex-col items-center text-center gap-16">
           <div className="flex flex-col items-center text-center gap-6 max-w-[700px]">
-            <h2 className="text-4xl font-bold text-[#FFFFFF]">{data.context_title}</h2>
-            <PrismicRichText field={data.context_text} components={{
-              paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
-            }}/>
+
+            {isFilled.richText(data.context_title) && (
+              <PrismicRichText 
+                field={data.context_title} 
+                components={{
+                  heading2: ({ children }) => (
+                    <h2 className="text-4xl font-bold text-[#FFFFFF]">{children}</h2>
+                  ),
+                }}
+              />
+            )}
+            
+            {isFilled.richText(data.context_text) && (
+              <PrismicRichText 
+                field={data.context_text} 
+                components={{
+                  paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
+                }}
+              />
+            )}
           </div>
         </Container>
       </section>
 
-      {/* Opps */}
-      <section className="bg-[#151515] py-[80px]">
+      {/* Opportunities */}
+      <section id="opportunities" className="bg-[#151515] py-[60px]">
         <Container size="xl" className="flex flex-col items-start gap-16">
-            <h2 className="text-4xl font-bold text-[#FFFFFF]">{data.opportunities_title}</h2>
+
+            {isFilled.richText(data.opportunities_title) && (
+              <PrismicRichText 
+                field={data.opportunities_title}
+                components={{
+                  heading2: ({ children }) => (
+                    <h2 className="text-4xl font-bold text-[#FFFFFF]">{children}</h2>
+                  ),
+                }}
+              />
+            )}
+
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-              {data.opportunity.map((item, index) => (
-                <div key={index} className="bg-[#1F1F1F] rounded-xl p-[48px] gap-4 flex flex-col items-start">
-                  <div className="w-[48px] h-[48px] rounded-lg bg-[#59b6f833] flex justify-center items-center">
-                    <div className="w-[24px] h-[24px] bg-[#59b6f8] rounded-2xl"></div>
+              {data.opportunity.map((item, index) => {
+                // get the correct icon component
+                const IconComponent = item.icon ? iconsMap[item.icon] : null;
+                return (
+                  <div key={index} className="bg-[#1F1F1F] rounded-xl p-[48px] gap-4 flex flex-col items-start">
+
+                    {IconComponent && (
+                      <IconComponent className="w-12 h-12 text-[#59B5F8]" />
+                    )}
+  
+                    {isFilled.richText(item.title) && (
+                      <PrismicRichText 
+                        field={item.title} 
+                        components={{
+                          heading3: ({ children }) => <h3 className="text-xl font-semibold text-[#FFFFFF]">{children}</h3>,
+                        }}
+                      />
+                    )}
+                    
+                    {isFilled.richText(item.text) && (
+                      <PrismicRichText 
+                        field={item.text} 
+                        components={{
+                          paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
+                        }}
+                      />
+                    )}
                   </div>
-                  <h3 className="text-2xl font-bold text-[#FFFFFF]">{item.title}</h3>
-                  <PrismicRichText field={item.text} components={{
-                      paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
-                  }}/>
-                </div>
-              ))}
+                )
+              })}
             </div>
         </Container>
       </section>
 
       {/* Data */}
-      <section className="bg-[#151515] py-[80px]">
+      <section id="data" className="relative bg-[#151515] py-[60px] pb-[200px] md:pb-[350px]">
+        <div className="absolute bottom-0 left-0 bg-[#FFFFFF] w-full">
+          <BottomAnimation></BottomAnimation>
+        </div>
+        <div className="relative">
+
         <Container size="xl" className="flex flex-col items-center text-center gap-16">
            <div className="flex flex-col items-center text-center gap-6 max-w-[700px]">
-            <h2 className="text-4xl font-bold text-[#FFFFFF]">{data.data_title}</h2>
-            <PrismicRichText field={data.data_text} components={{
-              paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
-            }}/>
+
+            {isFilled.richText(data.data_title) && (
+              <PrismicRichText 
+                field={data.data_title}
+                components={{
+                  heading2: ({ children }) => (
+                    <h2 className="text-4xl font-bold text-[#FFFFFF]">{children}</h2>
+                  ),
+                }}
+              />
+            )}
+
+            {isFilled.richText(data.data_text) && (
+              <PrismicRichText 
+                field={data.data_text} 
+                components={{
+                  paragraph: ({ children }) => <p className="text-[#A4A4A4]">{children}</p>,
+                }}
+              />
+            )}
           </div>
 
             <div className="border border-solid border-[#ffffff32] rounded-2xl overflow-hidden">
@@ -125,14 +246,23 @@ export default async function Home({
                 <tbody>
                     {data.generated_page.map((item, index) => (
                       <tr key={index}>
-                        <th key={item.company_name} className="p-4 border border-[#ffffff32] align-top text-left">{item.company_name}</th>
-                        <td key={item.role} className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.role}</td>
-                        <td key={item.challenges} className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.challenges}</td>
-                        <td key={item.pain_point} className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.pain_point}</td>
-                        <td key={item.industry_information} className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.industry_information}</td>
-                        <td key={item.key_message} className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.key_message}</td>
-                        <td key={item.page.text} className="p-4 border border-[#ffffff32] text-sm align-top text-left">
-                          <PrismicNextLink field={item.page}/>
+                        <th className="p-4 border border-[#ffffff32] align-top text-left">{item.company_name}</th>
+                        <td className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.role}</td>
+                        <td className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.challenges}</td>
+                        <td className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.pain_point}</td>
+                        <td className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.industry_information}</td>
+                        <td className="p-4 border border-[#ffffff32] text-[#A4A4A4] text-sm align-top text-left">{item.key_message}</td>
+                        <td className="p-4 border border-[#ffffff32] text-sm align-center text-left">
+                          {isFilled.keyText(item.page_link) && (
+                            <a 
+                              href={item.page_link} 
+                              target="_blank" 
+                              rel="noopener noreferrer" 
+                              className="text-[#FFFFFF] px-4 py-2 rounded-lg border-2 border-[#8E44EC]"
+                            >
+                              Open
+                            </a>
+                          )}
                         </td>
                       </tr>
                     ))}
@@ -140,25 +270,48 @@ export default async function Home({
               </table>
             </div>
         </Container>
+        </div>
       </section>
+        
 
       {/* Next steps */}
-      <section className="bg-[#FFFFFF] py-[80px]">
-        <Container size="xl" className="flex flex-col items-center text-center gap-16">
-          <div className="flex flex-col gap-8 items-center rounded-t-xl border-t-2 border-r-2 border-l-2 border-solid border-[#EEEEEE] p-[56px] bg-gradient-to-b from-[#EEEEEE] to-[#FFFFFF]z">
-            <div className="flex flex-col gap-8 items-center rounded-2xl p-[48px] bg-gradient-to-b from-[#D4F2E9] to-[#E8F8F3]">
-              <div className="flex flex-col items-center gap-4">
-                <p className="text-[#3BBB96]">{data.next_eyebrow}</p>
-                <h2 className="text-4xl font-bold">{data.next_title}</h2>
-              </div>
-              <p className="text-[#505050]">{data.next_text}</p>
+      <section className="bg-[#FFFFFF] pb-[60px]">
+        <Container size="xl" className="flex flex-col items-center gap-16 text-center">
+          <div className="w-full max-w-[700px] flex flex-col items-center gap-6">
+            <div className="flex flex-col items-center gap-4">
+              <p className="text-[#3BBB96]">{data.next_eyebrow}</p>
+                  
+              {isFilled.richText(data.next_title) && (
+                <PrismicRichText 
+                  field={data.next_title}
+                  components={{
+                    heading2: ({ children }) => (
+                      <h2 className="text-4xl font-bold">{children}</h2>
+                    ),
+                  }}
+                />
+              )}
+            </div>
+            <div className="flex flex-col gap-6 text-left">
+              {isFilled.richText(data.next_text) && (
+                <PrismicRichText 
+                  field={data.next_text}
+                  components={{
+                    heading3: ({ children }) => ( <h3 className="text-xl font-semibold text-[#151515]">{children}</h3>),
+                    heading4: ({ children }) => ( <h4 className="text-lg font-semibold text-[#151515]">{children}</h4>),
+                    paragraph: ({ children }) => <p className="text-[#505050]">{children}</p>,
+                    listItem: ({ children }) => <li className="text-[#505050] list-disc list-inside">{children}</li>,
+                    oListItem: ({ children }) => <li className="text-[#505050] list-decimal list-inside mb-4">{children}</li>,
+                  }}
+                />
+              )}
             </div>
             <div className="flex gap-4">
-              {data.buttons.map((link) => (
+              {data.buttons.map((link, index) => (
                 <PrismicNextLink
-                  key={link.key}
+                  key={index}
                   field={link}
-                  className={link.variant}
+                  className={`${link.variant === "Filled" ? "bg-[#151515] text-[#FFFFFF]" : "text-[#151515]"} px-4 py-2 rounded-lg border-2 border-[#151515]`}
                 />
               ))}
             </div>
